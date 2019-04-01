fdef78b1ba85fcdeb414b3e42741515a00044155
opp0615 <opp0615@naver.com>
2019-04-01 09:57:29 +0900

    15958 start:

---
+++ BaeckJoon/15958/15958/prodo.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+class Rect {
+public:
+	int width;
+	int height;
+};
+
+int main()
+{
+	int N = 0;
+	
+	cin >> N;
+	
+
+	for (int i = 0; i < N; i++) {
+		
+		int x, y = 0;
+		
+		cin >> x >> y;
+
+		
+		
+	}
+
+	return 0;
+}
