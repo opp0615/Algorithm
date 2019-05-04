43c25a9451077a8ff014708bd3b15d37386a1825
Jaeseok Yoo <opp0615@naver.com>
2019-05-04 17:47:53 +0900

    hw1 finish

--- SW역량테스트/hw/hw1/hw1/hw1.cpp
+++ SW역량테스트/hw/hw1/hw1/hw1.cpp
@@ -3,6 +3,7 @@
 
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
@@ -11,9 +12,13 @@ public:
 	int start;
 	int end;
 	int workTime;
+
+	bool operator <(Job& job) {
+		return this->end < job.end;
+	}
 };
 
-void WorkBatchProccess(vector<Job> jobs,int N, int finalDay);
+bool WorkBatchProccess(vector<Job> jobs,int N, int finalDay);
 
 
 int main()
@@ -47,42 +52,88 @@ int main()
 
 		}
 
-
 		//배치 프로세스
-		WorkBatchProccess(jobs,N, finalDay);
+		bool result = WorkBatchProccess(jobs,N, finalDay);
 
+		string resultText = result ? "Yes" : "No";
+
+		cout << resultText << endl;
 
 	}
 
 	return 0;
 }
 
-void WorkBatchProccess(vector<Job> jobs, int N, int finalDay)
+bool WorkBatchProccess(vector<Job> jobs, int N, int finalDay)
 {
-	vector<int> workQueue;
+	//작업 큐
+	vector<Job> workQueue;
 	
 
 	//1일부터 끝나는 날까지 진행
 	for (int day = 1; day < finalDay+1; day++) 
 	{
-
+		//하루에 8시간 일할 수 있음
+		int dayWorkTime = 8;
+		
 		//시작날이 지금 날과 같은 경우에 작업 배치
 		for (int j = 0; j < N; j++)
 		{
 			if (jobs[j].start == day) 
 			{
-				cout << jobs[j].start << ", " << jobs[j].end << ", " << jobs[j].workTime << endl;
-				workQueue.push_back(j);
-			}
 				
+				workQueue.push_back(jobs[j]);
+			}
+		}
+
+		//끝날이 이른 순서대로 정렬
+		sort(workQueue.begin(), workQueue.end());
+
+
+		//정렬된 큐를 돌면서 하루 8시간씩 작업
+		for (vector<Job>::iterator iter = workQueue.begin(); iter != workQueue.end(); )
+		{
+			//cout << iter->start << ", " << iter->end << ", " << iter->workTime<<endl;
+
+			//남은 일할 수 있는 시간 보다 일해야 하는 시간이 클 때
+			if (iter->workTime > dayWorkTime)
+			{
+				iter->workTime -= dayWorkTime;
+				break;
+			}
+			//남은 일할 수 있는 시간보다 일해야 하는 시간이 작거을 때
+			else if(iter->workTime < dayWorkTime)
+			{
+				dayWorkTime -= iter->workTime;
+
+				//해당 작업 지우고 다음 작업으로
+				iter = workQueue.erase(iter);
+			}
+			//남을 일할 수 있는 시간과 일해야하는 시간이 같을 때
+			else 
+			{
+				iter = workQueue.erase(iter);
+				break;
+			}
+
 		}
 
 	}
 
+	//작업을 다 처리했으면 true
+	if (workQueue.size() == 0)
+	{
+		return true;
+	}
+	//못했으면 false
+	else {
+		return false;
+	}
 
 }
 
 /*
+테스트 케이스
 3
 3
 1 3 8
